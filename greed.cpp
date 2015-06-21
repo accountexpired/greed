4ce541ff4810df52381decbcc5735a873543eebb
dhf <daghenrik@gmail.com>
2015-06-21 14:41:52 +0200

    Rewrite to enable non-blocking I/O using ncurses. Also enable ncurses-based coloring.

--- greed.cpp
+++ greed.cpp
@@ -1,3 +1,4 @@
+#include <chrono>
 #include <iostream>
 #include <iterator>
 #include <map>
@@ -5,83 +6,13 @@
 #include <stdexcept>
 #include <vector>
 
-std::string color(std::string hue)
-{
-    static const std::map<std::string, int> hues = {
-        {"default",  0},
-        {"black",   30},
-        {"red",     31},
-        {"green",   32},
-        {"yellow",  33},
-        {"blue",    34},
-        {"magenta", 35},
-        {"cyan",    36},
-        {"white",   37}
-    };
+#include <ncurses.h>
 
-    std::map<std::string, int>::const_iterator i = hues.find(hue);
+#include "item.h"
+#include "creature.h"
+#include "room.h"
 
-    if (i == hues.end()){
-        return "";
-    } else if (i->first == "default") {
-        return "\033[0m";
-    }
-
-    return "\033[;" + std::to_string(hues.find(hue)->second) + "m";
-}
-
-class Item {
-public:
-    std::string name;
-    std::string desc;
-};
-
-class Creature {
-public:
-    std::string name;
-    std::string desc;
-    std::vector<std::unique_ptr<Item>> items;
-};
-
-class Room {
-public:
-    std::string desc;
-    std::map<std::string, Room *> exits;
-    std::vector<std::unique_ptr<Item>> items;
-    std::vector<std::unique_ptr<Creature>> creatures;
-    void display() const;
-};
-
-void Room::display() const
-{
-    std::cout << desc << std::endl;
-
-    // Display creatures in the room.
-    for (std::vector<std::unique_ptr<Creature>>::const_iterator creature = creatures.begin();
-         creature != creatures.end(); ++creature) {
-        std::cout << color("red") << (*creature)->name << color("default") << std::endl;
-    }
-
-    // Display items in the room.
-    for (std::vector<std::unique_ptr<Item>>::const_iterator item = items.begin();
-         item != items.end(); ++item) {
-        std::cout << color("yellow") << (*item)->name << color("default") << std::endl;
-    }
-
-    // Display exits from the room.
-    std::cout << "Exits: ";
-    for (std::map<std::string, Room *>::const_iterator exit = exits.begin();
-         exit != exits.end(); ++exit) {
-        std::cout << exit->first;
-        if (std::distance(exit, exits.end()) > 1)
-        {
-            std::cout << ", ";
-        }
-    }
-    std::cout << "." << std::endl;
-}
-
-std::vector<Room> create_area()
+static std::vector<Room> create_area()
 {
     // TODO: Generate areas from an external source, e.g. an SQLite DB-file.
     std::vector<Room> area(3);
@@ -119,26 +50,70 @@ std::vector<Room> create_area()
     return area;
 }
 
+static void init_ncurses()
+{
+    initscr();
+
+    // When we reach the bottom of the screen, the existing text
+    // should scroll upwards, just like on a real terminal.
+    scrollok(stdscr, TRUE);
+
+    // Enable non-blocking input.
+    nodelay(stdscr, TRUE);
+
+    start_color();
+    init_pair(1, COLOR_RED, COLOR_BLACK);
+    init_pair(2, COLOR_YELLOW, COLOR_BLACK);
+}
+
 int main(void)
 {
-    std::vector<Room> area = create_area();
-
-    Room *currentRoom = &area[0];
+    int ch;
     std::string kbdInput;
+    std::vector<Room> area = create_area();
+    Room *currentRoom = &area[0];
+
+    init_ncurses();
 
     currentRoom->display();
-    std::cout << "> ";
+    printw("> ");
 
-    while (std::cin >> kbdInput) {
-        // If the input was a direction, go there and switch to the corresponding room.
-        if (currentRoom->exits.find(kbdInput) != currentRoom->exits.end()) {
-            currentRoom = currentRoom->exits[kbdInput];
-            currentRoom->display();
-        } else {
-            std::cout << "What?" << std::endl;
+    auto start = std::chrono::steady_clock::now();
+    std::chrono::seconds tick(2); // Each tick should last two seconds.
+
+    while (true)
+    {
+        // Handle all timed events, such as combat.
+        if ((std::chrono::steady_clock::now() - start) > tick)
+        {
+            start = std::chrono::steady_clock::now();
+            printw("Tick! Tock!\n");
         }
-        std::cout << "> ";
+
+        // Handle user input without blocking.
+        if ((ch = getch()) != ERR)
+        {
+            if (ch == '\n')
+            {
+                if (currentRoom->exits.find(kbdInput) != currentRoom->exits.end())
+                {
+                    currentRoom = currentRoom->exits[kbdInput];
+                    currentRoom->display();
+                } else
+                {
+                    printw("What?\n");
+                }
+
+                printw("> ");
+                kbdInput.clear();
+            } else
+            {
+            kbdInput.push_back(ch);
+            }
+        }
     }
+
+    endwin();
 
     return 0;
 }
